081d4765e31d3cb45c19ae0f1cf2be3635ceaf3d
rizbutt86 <rizwanbutt86@gmail.com>
2025-05-27 16:53:44 -0400

    fix: rename JSX files and update imports

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero.jsx'
+import Section from './components/Section.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Section />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
